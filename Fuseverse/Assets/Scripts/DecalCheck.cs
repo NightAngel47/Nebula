6e8f4dc4c284c8979d07a43708038ffe0b119fc5
probopastor <43503592+probopastor@users.noreply.github.com>
2019-02-05 13:39:09 -0600

    Kinematc rigidbody on decals
    that

--- Fuseverse/Assets/Scripts/DecalCheck.cs
+++ Fuseverse/Assets/Scripts/DecalCheck.cs
@@ -4,35 +4,19 @@ using UnityEngine;
 
 public class DecalCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("fj");
+        Debug.Log("trigger");
 
         if ((other.tag != gameObject.tag))
         {
             if (other.tag != "Planet")
-            { }
+            {
                 Destroy(other.gameObject);
-            
+            }
+
 
         }
-        
-    }
-
-    private void Awake()
-    {
         
     }
 }
