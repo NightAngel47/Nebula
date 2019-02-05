b85d4318bff19a0f1d922edbd4556299b7a52dcf
probopastor <43503592+probopastor@users.noreply.github.com>
2019-02-05 13:22:32 -0600

    iyg
    8ihyo

---
+++ Fuseverse/Assets/Scripts/DecalCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("fj");
+
+        if ((other.tag != gameObject.tag))
+        {
+            if (other.tag != "Planet")
+            { }
+                Destroy(other.gameObject);
+            
+
+        }
+        
+    }
+
+    private void Awake()
+    {
+        
+    }
+}
