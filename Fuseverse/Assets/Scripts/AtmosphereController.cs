22f2b500a5cdd21755ba32758a967423c602c2d1
probopastor <43503592+probopastor@users.noreply.github.com>
2019-02-16 02:34:56 -0600

    Atmosphere Editing On Pinch
    Atmosphere can now be changed via touch screen. Pinch outwards to increase density and pinch inwards to decrease density.
    
    Old atmosphere script was renamed AtmosphereControllerTest. This script continues to use A to increase atmospheric density and S to decrease it.

--- Fuseverse/Assets/Scripts/AtmosphereController.cs
+++ Fuseverse/Assets/Scripts/AtmosphereController.cs
@@ -6,10 +6,16 @@ public class AtmosphereController : MonoBehaviour
 {
     public Renderer rend;
     
+    //Replacement color of shader
     Color shaderColor;
+
+    //Current shader color
     Color originalColor;
+
+    //Original alpha channel value
     float originalAlpha;
 
+    //Value to increment alpha value by
     float alphaIncrement = 0f;
     
 
@@ -19,12 +25,14 @@ public class AtmosphereController : MonoBehaviour
     {
 
         rend = GetComponent<Renderer>();
- 
+        
+        //Set shaderColor equal to original color
         originalColor = GetComponent<Renderer>().material.color;
         shaderColor = new Color(originalColor.r, originalColor.g, originalColor.b, originalColor.a);
 
         originalAlpha = originalColor.a;
 
+        //alphaIncrement starts at value 0 on start
         alphaIncrement = 0f;
 
 
@@ -33,54 +41,74 @@ public class AtmosphereController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.touchCount == 2)
         {
-            alphaIncrement += 0.01f;
-            float newAlpha = (originalAlpha + alphaIncrement);
+            //Get current touch positions
+            Touch firstTouch = Input.GetTouch(0);
+            Touch secondTouch = Input.GetTouch(1);
 
-            
-            if (newAlpha > 1f)
+            //Find previous touch positions
+            Vector2 firstTouchPrevPos = firstTouch.position - firstTouch.deltaPosition;
+            Vector2 secondTouchPrevPos = secondTouch.position - secondTouch.deltaPosition;
+
+            //Find magnitude of previous touch positions
+            float prevTouchDeltaMag = (firstTouchPrevPos - secondTouchPrevPos).magnitude;
+
+            //Find magnitude of current touch positions
+            float touchDeltaMag = (firstTouch.position - secondTouch.position).magnitude;
+
+            //Find difference between the current and previous magnitude
+            float deltaMagDif = touchDeltaMag - prevTouchDeltaMag;
+
+            if(deltaMagDif > 0)
             {
-                newAlpha = 1f;
+                Debug.Log(">0");
+
+                alphaIncrement += 0.01f;
+                float newAlpha = (originalAlpha + alphaIncrement);
+
+                //Keeps alpha from hitting a value above 1 and below 0 on slider 
+                if (newAlpha > 1f)
+                {
+                    newAlpha = 1f;
+                }
+                if (newAlpha < 0f)
+                {
+                    newAlpha = 0f;
+                }
+
+                //Creates color with new alpha and sets material to color
+                shaderColor = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, newAlpha);
+                rend.material.SetColor("_BaseColor", shaderColor);
+
+
+                Debug.Log("Alpha Change" + newAlpha);
             }
-            if (newAlpha < 0f)
+            else if(deltaMagDif < 0)
             {
-                newAlpha = 0f;
+                Debug.Log("<0");
+
+                alphaIncrement -= 0.01f;
+                float newAlpha = (originalAlpha + alphaIncrement);
+
+                //Keeps alpha from hitting a value above 1 and below 0 on slider 
+                if (newAlpha < 0f)
+                {
+                    newAlpha = 0f;
+                }
+                if (newAlpha > 1f)
+                {
+                    newAlpha = 1f;
+                }
+
+                //Creates color with new alpha and sets material to color
+                shaderColor = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, newAlpha);
+                rend.material.SetColor("_BaseColor", shaderColor);
+
+
+                Debug.Log("Alpha Change" + newAlpha);
             }
 
-
-            shaderColor = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, newAlpha);
-
-
-            rend.material.SetColor("_BaseColor", shaderColor);
-
-
-            //alphaIncrement += 0.01f;
-            Debug.Log("Alpha Change" + newAlpha);
-        }
-
-        else if (Input.GetKey(KeyCode.S))
-        {
-            alphaIncrement -= 0.01f;
-            float newAlpha = (originalAlpha + alphaIncrement);
-
-
-            if (newAlpha < 0f)
-            {
-                newAlpha = 0f;
-            }
-            if (newAlpha > 1f)
-            {
-                newAlpha = 1f;
-            }
-
-            shaderColor = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, newAlpha);
-
-
-            rend.material.SetColor("_BaseColor", shaderColor);
-
-
-            Debug.Log("Alpha Change" + newAlpha);
         }
 
     }
---
+++ Fuseverse/Assets/Scripts/AtmosphereControllerTest.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtmosphereControllerTest : MonoBehaviour
+{
+    public Renderer rend;
+
+    Color shaderColor;
+    Color originalColor;
+    float originalAlpha;
+
+    float alphaIncrement = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rend = GetComponent<Renderer>();
+
+        originalColor = GetComponent<Renderer>().material.color;
+        shaderColor = new Color(originalColor.r, originalColor.g, originalColor.b, originalColor.a);
+
+        originalAlpha = originalColor.a;
+
+        alphaIncrement = 0f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            alphaIncrement += 0.01f;
+            float newAlpha = (originalAlpha + alphaIncrement);
+
+
+            if (newAlpha > 1f)
+            {
+                newAlpha = 1f;
+            }
+            if (newAlpha < 0f)
+            {
+                newAlpha = 0f;
+            }
+
+
+            shaderColor = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, newAlpha);
+
+
+            rend.material.SetColor("_BaseColor", shaderColor);
+
+
+            //alphaIncrement += 0.01f;
+            Debug.Log("Alpha Change" + newAlpha);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            alphaIncrement -= 0.01f;
+            float newAlpha = (originalAlpha + alphaIncrement);
+
+
+            if (newAlpha < 0f)
+            {
+                newAlpha = 0f;
+            }
+            if (newAlpha > 1f)
+            {
+                newAlpha = 1f;
+            }
+
+            shaderColor = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, newAlpha);
+
+
+            rend.material.SetColor("_BaseColor", shaderColor);
+
+
+            Debug.Log("Alpha Change" + newAlpha);
+        }
+
+    }
+}
+
