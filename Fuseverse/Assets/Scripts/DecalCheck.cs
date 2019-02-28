08745ca584db74a8528bc4e6eeacec2ccb9058b5
Steven <NightAngel47@users.noreply.github.com>
2019-02-28 12:36:08 -0600

    IDK
    Can you do the thing

---
+++ Fuseverse/Assets/Scripts/DecalCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalCheck : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger");
+
+        if ((other.tag != gameObject.tag))
+        {
+            if (other.tag != "Planet")
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
