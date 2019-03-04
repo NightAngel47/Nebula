8060d8d929495ef903fb562f31dc551fae002e8e
probopastor <43503592+probopastor@users.noreply.github.com>
2019-03-03 21:45:43 -0600

    Compass Cube Rotates
    Compass Cube added to compass in Gas Giant Creator scene. Rotates with planet, is set back to its original position and rotation upon clicking Rotation 0 Button.
    
    Rotation 0 Button in Gas Giant Creator scene given its own Canvas and set to World Space to make this work. This is only in the Gas Giant Creator scene, so that the feature can be easily reverted to the settings of the Terrestrial Creator Scene if it is disliked.

---
+++ Fuseverse/Assets/Scripts/CompassRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassRotation : MonoBehaviour
+{
+    Vector3 defaultPosition;
+    Quaternion defaultRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetRotation()
+    {
+        //Resets position to default
+        Vector3 defaultPosition = new Vector3(-0.01f, 0.97f, 0f);
+        gameObject.transform.position = defaultPosition;
+
+        //Resets rotation to default
+        Quaternion defaultRotation = Quaternion.Euler(0, 0, 0);
+        gameObject.transform.rotation = defaultRotation;
+
+    }
+}
