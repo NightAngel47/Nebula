793a2baba9a766c50474a3b91affe095848f7597
FoshyTakashi <42752172+FoshyTakashi@users.noreply.github.com>
2019-03-11 13:19:47 -0500

    gas UI started

---
+++ Fuseverse/Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    public GameObject rotatehands;
+
+    private float inputTimer;
+    void Start()
+    {
+        inputTimer = 0;
+    }
+    void Update()
+    {
+        inputTimer += Time.deltaTime;
+        //INPUT CHECK
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inputTimer = 0;
+            rotatehands.SetActive(false);
+        }
+        if (inputTimer >= 10f)
+        {
+            rotatehands.SetActive(true);
+        }
+    }
+}
