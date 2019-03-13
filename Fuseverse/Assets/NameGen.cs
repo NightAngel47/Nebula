e558bd8b52cf5482872823d8d754b8e75f0471f8
FoshyTakashi <42752172+FoshyTakashi@users.noreply.github.com>
2019-03-13 12:23:57 -0500

    Confimation Screens and Gas Refinment

---
+++ Fuseverse/Assets/NameGen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NameGen : MonoBehaviour
+{
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Random Name";
+    }
+    /* Name List
+ * Sirius
+ * Canopus
+ * Alpha Centauri
+ * Arcturus
+ * Vega
+ * Capella
+ * Regel
+ * Procyon
+ * Archernar
+ * Beta Centauri
+ * Altair
+ * Betelgeuse
+ * Aldebaran
+ * Alpha Crucis
+ * Spica
+ * Antares
+ * Pollux
+ * Fomalhaut
+ * Deneb
+ * Beta Crucis
+ * Regulus
+ */
+}
--- Fuseverse/Assets/Scripts/Main Menu & UI/RestartButtonScript.cs
+++ Fuseverse/Assets/Scripts/Main Menu & UI/RestartButtonScript.cs
@@ -1,28 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RestartButtonScript : MonoBehaviour
 {
     public Button ExitButton;
     public Button FinishButton;
+    public Button ConfirmExit;
+    public Button ConfirmFinish;
+    public Button CancelExit;
+    public Button CancelFinish;
+
+    public GameObject verifyExit;
+    public GameObject verifyFinish;
+    public GameObject OtherUI1;
+    public GameObject OtherUI2;
+
 
     void Start()
     {
+        verifyExit.SetActive(false);
+        verifyFinish.SetActive(false);
+
         ExitButton.onClick.AddListener(TaskOnClickExit);
         FinishButton.onClick.AddListener(TaskOnClickFinish);
-
+        ConfirmExit.onClick.AddListener(TaskOnClickConfirmExit);
+        ConfirmFinish.onClick.AddListener(TaskOnClickConfirmFinish);
+        CancelExit.onClick.AddListener(TaskOnClickCancelExit);
+        CancelFinish.onClick.AddListener(TaskOnClickCancelFinish);
     }
 
-    // Update is called once per frame
     void TaskOnClickExit()
     {
-        SceneManager.LoadScene(sceneName: "Main Menu");
+        verifyExit.SetActive(true);
+        OtherUI1.SetActive(false);
+        OtherUI2.SetActive(false);
     }
     void TaskOnClickFinish()
     {
-        SceneManager.LoadScene(sceneName: "Complete Screen");
+        verifyFinish.SetActive(true);
+        OtherUI1.SetActive(false);
+        OtherUI2.SetActive(false);
+    }
+
+    void TaskOnClickCancelExit()
+    {
+        verifyExit.SetActive(false);
+        verifyFinish.SetActive(false);
+        OtherUI1.SetActive(true);
+        OtherUI2.SetActive(true);
+    }
+    void TaskOnClickCancelFinish()
+    {
+        verifyExit.SetActive(false);
+        verifyFinish.SetActive(false);
+        OtherUI1.SetActive(true);
+        OtherUI2.SetActive(true);
+    }
+    void TaskOnClickConfirmFinish()
+    {
+        SceneManager.LoadScene("Finish Screen");
+    }
+    void TaskOnClickConfirmExit()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 }
