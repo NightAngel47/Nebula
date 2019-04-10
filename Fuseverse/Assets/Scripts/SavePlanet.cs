65f611803c072d1e0827d0ebe0bb9f794563a8fd
Steven <NightAngel47@users.noreply.github.com>
2019-04-10 12:10:42 -0500

    Worked on Biomes and Terrain
    Updating them to get them back to working like before.

--- Fuseverse/Assets/Scripts/SavePlanet.cs
+++ Fuseverse/Assets/Scripts/SavePlanet.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class SavePlanet : MonoBehaviour
 {
     public Transform rings;
+    public RenderTexture canvasTexture;
+    public Material baseMaterial;
 
     private void Start()
     {
@@ -47,5 +49,36 @@ public class SavePlanet : MonoBehaviour
                 rings.localScale = GameObject.FindGameObjectWithTag("FinishPos").transform.localScale;
             }
         }
+    }
+
+    //https://codeartist.mx/dynamic-texture-painting/
+
+    public void SaveTexture()
+    {
+        System.DateTime date = System.DateTime.Now;
+        RenderTexture.active = canvasTexture;
+        Texture2D tex = new Texture2D(canvasTexture.width, canvasTexture.height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, canvasTexture.width, canvasTexture.height), 0, 0);
+        tex.Apply();
+        RenderTexture.active = null;
+        baseMaterial.mainTexture = tex; //Put the painted texture as the base
+        //foreach (Transform child in brushContainer.transform)
+        //{//Clear brushes
+        //    Destroy(child.gameObject);
+        //}
+        StartCoroutine(SaveTextureToFile(tex)); // save the texture
+    }
+
+    IEnumerator SaveTextureToFile(Texture2D savedTexture)
+    {
+        string fullPath = System.IO.Directory.GetCurrentDirectory() + "\\UserCanvas\\";
+        System.DateTime date = System.DateTime.Now;
+        string fileName = "CanvasTexture.png";
+        if (!System.IO.Directory.Exists(fullPath))
+            System.IO.Directory.CreateDirectory(fullPath);
+        var bytes = savedTexture.EncodeToPNG();
+        System.IO.File.WriteAllBytes(fullPath + fileName, bytes);
+        Debug.Log("<color=orange>Saved Successfully!</color>" + fullPath + fileName);
+        yield return null;
     }
 }
