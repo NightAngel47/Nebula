38f0cec71785e58c79dc42bf5321fc06c963e2bf
Steven <NightAngel47@users.noreply.github.com>
2019-04-11 23:16:52 -0500

    Default Biome Works
    5 random default biomes working.

---
+++ Fuseverse/Assets/Scripts/DefaultBiome.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultBiome : MonoBehaviour
+{
+    public Material mat;
+    public string defaultBiome;
+    public Texture[] biomeTexs;
+    public string[] biomeTags;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        SetDefaultBiome();
+    }
+
+    void SetDefaultBiome()
+    {
+        int randNum = Random.Range(0, biomeTags.Length);
+
+        mat.SetTexture("_texture", biomeTexs[randNum]);
+        defaultBiome = biomeTags[randNum];
+    }
+}
