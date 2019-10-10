2996cf5132c46725e505d658d2f5dfea3aa4f7ff
probopastor <43503592+probopastor@users.noreply.github.com>
2019-10-10 01:36:13 -0500

    Gas Rotation Debug

--- Nebula/Assets/Scripts/Planets/Gas Giant/GasRotation.cs
+++ Nebula/Assets/Scripts/Planets/Gas Giant/GasRotation.cs
@@ -6,6 +6,9 @@ public class GasRotation : MonoBehaviour
 {
     public AudioSource source;
 
+    [SerializeField] private float gasRotationSpeedDebug;
+    [SerializeField] private float gasRotationFastSpeedDebug;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,22 @@ public class GasRotation : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if(DebugController.debugEnabled && !RingRotation.canRingRotate)
+        {
+            if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+            {
+                DebugGasRotator();
+            }
+        }
+
+        if ((Input.touchCount == 2) && !RingRotation.canRingRotate)
+        {
+            GasRotator();
+        }
+    }
+
+    void GasRotator()
     {
         var xRot = Camera.main.transform.rotation.x;
         var yRot = Camera.main.transform.rotation.y;
@@ -28,13 +47,124 @@ public class GasRotation : MonoBehaviour
                 yRot += (-touchDeltaPosition.x);
 
                 if (!source.isPlaying)
+                {
                     source.Play();
+                }
 
                 transform.Rotate(xRot, yRot, 0, Space.World);
             }
-
         }
         else
+        {
             source.Stop();
+        }
+    }
+
+    void DebugGasRotator()
+    {
+        float rotationSpeed = gasRotationSpeedDebug;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rotationSpeed = gasRotationFastSpeedDebug;
+        }
+        else
+        {
+            rotationSpeed = gasRotationSpeedDebug;
+        }
+
+        var xRot = Camera.main.transform.rotation.x;
+        var yRot = Camera.main.transform.rotation.y;
+        //var zRot = Camera.main.transform.rotation.z;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+                yRot -= rotationSpeed;
+
+                if (!source.isPlaying)
+                {
+                    source.Play();
+                }
+
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                xRot += rotationSpeed;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                xRot -= rotationSpeed;
+            }
+
+            transform.Rotate(xRot, yRot, 0, Space.World);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            yRot += rotationSpeed;
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                xRot += rotationSpeed;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                xRot -= rotationSpeed;
+            }
+
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+
+            transform.Rotate(xRot, yRot, 0, Space.World);
+        }
+        else if(Input.GetKey(KeyCode.UpArrow))
+        {
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                yRot += rotationSpeed;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                yRot -= rotationSpeed;
+            }
+
+
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+
+            xRot += rotationSpeed;
+
+            transform.Rotate(xRot, yRot, 0, Space.World);
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                yRot += rotationSpeed;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                yRot -= rotationSpeed;
+            }
+
+
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+
+            xRot -= rotationSpeed;
+
+            transform.Rotate(xRot, yRot, 0, Space.World);
+        }
+        else
+        {
+            source.Stop();
+        }
     }
 }
