271699c7c6c5f1762c53ef17217b1535d71e0477
probopastor <43503592+probopastor@users.noreply.github.com>
2019-10-10 02:54:06 -0500

    Ring Rotation / Size Control via Debug Controls
    Left arrow and right arrow increase / decrease particles in rings, up and down arrow roate them.
    
    Hold shift to rotate faster.

--- Nebula/Assets/Scripts/Planets/Gas Giant/RingRotation.cs
+++ Nebula/Assets/Scripts/Planets/Gas Giant/RingRotation.cs
@@ -8,16 +8,29 @@ public class RingRotation : MonoBehaviour
     public float rotationSpeed = 0;
     public static bool canRingRotate = false;
 
+    [SerializeField] private float gasRingRotationSpeedDebug;
+    [SerializeField] private float gasRingRotationFastSpeedDebug;
+
     // Update is called once per frame
     void Update()
     {
+        if(DebugController.debugEnabled)
+        {
+            if((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.UpArrow)) && canRingRotate)
+            {
+                RotateRingsDebug();
+                PlayAudio();
+            }
+        }
         if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && canRingRotate == true)
         {
             RotateRings();
             PlayAudio();
         }
         else
+        {
             source.Stop();
+        }
     }
 
     void RotateRings()
@@ -36,6 +49,34 @@ public class RingRotation : MonoBehaviour
         {
             xRot += (touchDeltaPosition.y * Time.deltaTime) * rotationSpeed;
 
+            transform.Rotate(xRot, 0, 0, Space.World);
+        }
+    }
+
+    void RotateRingsDebug()
+    {
+        float ringRotationSpeed = gasRingRotationSpeedDebug;
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            ringRotationSpeed = gasRingRotationFastSpeedDebug;
+        }
+        else
+        {
+            ringRotationSpeed = gasRingRotationSpeedDebug;
+        }
+
+        var xRot = Camera.main.transform.rotation.x;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            xRot -= ringRotationSpeed;
+
+            transform.Rotate(xRot, 0, 0, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            xRot += ringRotationSpeed;
             transform.Rotate(xRot, 0, 0, Space.World);
         }
     }
