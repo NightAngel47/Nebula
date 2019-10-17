c1217d0f5affccfd82fda35ffc016aa3be9425bc
probopastor <43503592+probopastor@users.noreply.github.com>
2019-10-17 17:50:28 -0500

    Debug Canvas for all scenes
    Debug canvas tells user whether or not game is in debug mode and whether or not rotation is set to the mouse. Hide this by pressing C.

---
+++ Nebula/Assets/DebugCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCanvas : MonoBehaviour
+{
+    public static DebugCanvas instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Nebula/Assets/Scripts/Debug/DebugController.cs
+++ Nebula/Assets/Scripts/Debug/DebugController.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class DebugController : MonoBehaviour
 {
     public static bool debugEnabled;
     public static bool mouseRotationEnabled;
+
+    private GameObject debugCanvas;
+    private Text debugText;
+    private Text mouseRotationText;
+
+    private bool debugCanvasEnabled;
 
     private bool currentlyEnabled;
 
@@ -20,6 +25,12 @@ public class DebugController : MonoBehaviour
             DontDestroyOnLoad(gameObject);
             debugEnabled = false;
             currentlyEnabled = false;
+            debugCanvasEnabled = true;
+
+            debugCanvas = GameObject.FindGameObjectWithTag("DebugCanvas");
+            debugText = GameObject.FindGameObjectWithTag("DebugModeText").GetComponent<Text>();
+            mouseRotationText = GameObject.FindGameObjectWithTag("DebugModeRotationText").GetComponent<Text>();
+
         }
         else if (instance != this)
         {
@@ -31,6 +42,11 @@ public class DebugController : MonoBehaviour
     void Update()
     {
         DebugCheck();
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            HideDebugCanvas();
+        }
     }
 
     /// <summary>
@@ -44,12 +60,16 @@ public class DebugController : MonoBehaviour
             {
                 debugEnabled = false;
                 currentlyEnabled = false;
+                mouseRotationEnabled = false;
+                DebugText();
+                MouseRotationText();
                 Debug.Log("Debug Mode Enabled " + debugEnabled);
             }
             else if(!currentlyEnabled)
             {
                 debugEnabled = true;
                 currentlyEnabled = true;
+                DebugText();
                 Debug.Log("Debug Mode Enabled " + debugEnabled);
             }
             
@@ -62,12 +82,52 @@ public class DebugController : MonoBehaviour
                 if(mouseRotationEnabled)
                 {
                     mouseRotationEnabled = false;
+                    MouseRotationText();
                 }
                 else if(!mouseRotationEnabled)
                 {
                     mouseRotationEnabled = true;
+                    MouseRotationText();
                 }
             }
+        }
+    }
+
+    private void DebugText()
+    {
+        if(debugEnabled)
+        {
+            debugText.text = "Debug Mode: True";
+        }
+        else if(!debugEnabled)
+        {
+            debugText.text = "Debug Mode: False";
+        }
+    }
+
+    private void MouseRotationText()
+    {
+        if(mouseRotationEnabled)
+        {
+            mouseRotationText.text = "Mouse Rotation: True";
+        }
+        else if(!mouseRotationEnabled)
+        {
+            mouseRotationText.text = "Mouse Rotation: False";
+        }
+    }
+
+    private void HideDebugCanvas()
+    {
+        if(debugCanvasEnabled)
+        {
+            debugCanvas.SetActive(false);
+            debugCanvasEnabled = false;
+        }
+        else if(!debugCanvasEnabled)
+        {
+            debugCanvas.SetActive(true);
+            debugCanvasEnabled = true;
         }
     }
 }
