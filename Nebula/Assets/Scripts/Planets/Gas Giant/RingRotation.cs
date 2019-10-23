9279fe42f87d2508dac6153a976d83921081d3fb
probopastor <43503592+probopastor@users.noreply.github.com>
2019-10-22 21:18:50 -0500

    moved timer.cs and ringrotation.cs stuff over

--- Nebula/Assets/Scripts/Planets/Gas Giant/RingRotation.cs
+++ Nebula/Assets/Scripts/Planets/Gas Giant/RingRotation.cs
@@ -8,6 +8,14 @@ public class RingRotation : MonoBehaviour
     public float rotationSpeed = 0;
     public static bool canRingRotate = false;
 
+    #region debug ring rotation
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+    [SerializeField] private float gasRingRotationSpeedDebug;
+    [SerializeField] private float gasRingRotationFastSpeedDebug;
+#endif
+    #endregion
+
+
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +25,22 @@ public class RingRotation : MonoBehaviour
             PlayAudio();
         }
         else
+        {
             source.Stop();
+        }
+
+        #region debug ring rotation
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+        if (DebugController.debugEnabled)
+        {
+            if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.UpArrow)) && canRingRotate)
+            {
+                RotateRingsDebug();
+                PlayAudio();
+            }
+        }
+#endif
+        #endregion
     }
 
     void RotateRings()
@@ -39,6 +62,38 @@ public class RingRotation : MonoBehaviour
             transform.Rotate(xRot, 0, 0, Space.World);
         }
     }
+
+    #region debug ring rotation
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+    void RotateRingsDebug()
+    {
+        float ringRotationSpeed = gasRingRotationSpeedDebug;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            ringRotationSpeed = gasRingRotationFastSpeedDebug;
+        }
+        else
+        {
+            ringRotationSpeed = gasRingRotationSpeedDebug;
+        }
+
+        var xRot = Camera.main.transform.rotation.x;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            xRot -= ringRotationSpeed;
+
+            transform.Rotate(xRot, 0, 0, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            xRot += ringRotationSpeed;
+            transform.Rotate(xRot, 0, 0, Space.World);
+        }
+    }
+#endif
+    #endregion
 
     void PlayAudio()
     {
