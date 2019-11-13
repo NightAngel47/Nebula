4d39de11bc03c3e50801b8edfea2609195b054bb
Steven <NightAngel47@users.noreply.github.com>
2019-11-13 17:56:16 -0600

    New Terrain Painting WIP
    Almost there. Terrain spawns based on biome placed, but the prefab spawned doesn't match. Will look into soon.

--- Nebula/Assets/Scripts/Planets/Terrestrial/TerrainPainter.cs
+++ Nebula/Assets/Scripts/Planets/Terrestrial/TerrainPainter.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TerrainSelect))]
 public class TerrainPainter : MonoBehaviour
 {
     #region References
@@ -43,17 +44,16 @@ public class TerrainPainter : MonoBehaviour
     /// <summary>
     /// The terrain options
     /// </summary>
-    private enum terrainTools
+    private enum TerrainTools
     {
-        none,
-        up,
-        plants,
-        erase
+        Plants,
+        Up,
+        Erase
     };
     /// <summary>
     /// The currently selected terrain tool
     /// </summary>
-    private terrainTools terrainToolSelected = terrainTools.plants;
+    private TerrainTools terrainToolSelected = TerrainTools.Plants;
     /// <summary>
     /// Selected Terrain object to spawn
     /// </summary>
@@ -70,36 +70,53 @@ public class TerrainPainter : MonoBehaviour
     {
         planet = GameObject.FindGameObjectWithTag("Planet");
         mainCam = Camera.main;
+        ts = GetComponent<TerrainSelect>();
     }
     
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1))
         {
             Vector3 cursorPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
-            SpawnTerrain(cursorPos);
+            Ray cursorRay = mainCam.ScreenPointToRay(cursorPos);
+            SpawnTerrain(cursorRay);
         }
     }
     
     /// <summary>
     /// Spawn terrain based on biome at inputPos
     /// </summary>
-    /// <param name="inputPos">The position of the input</param>
-    private void SpawnTerrain(Vector2 inputPos)
+    /// <param name="cursorRay">The ray from input</param>
+    private void SpawnTerrain(Ray cursorRay)
     {
-        Ray cursorRay = mainCam.ScreenPointToRay(inputPos);
-        
-        Vector3 uvWorldPosition = Vector3.zero;
-        string masterDecalTag = MaskNames.Green.ToString(); // default color of master
-        if (HitTestUVPosition(cursorRay, ref uvWorldPosition, maskPainterCams[(int) MaskNames.Master]))
+        DetermineTerrain(cursorRay);
+        SpawnTerrainOnPlanet(cursorRay);
+    }
+
+    /// <summary>
+    /// Determine the terrain to spawn based on terrain tool selected and biome hit
+    /// </summary>
+    /// <param name="cursorRay">The position of the hit</param>
+    private void DetermineTerrain(Ray cursorRay)
+    {
+        if (terrainToolSelected == TerrainTools.Erase)
         {
+            selectedTerrain = terrainEraser;
+        }
+        else
+        {
+            Vector3 uvWorldPosition = Vector3.zero;
+            string masterDecalTag = MaskNames.Green.ToString(); // default color of master
+            Vector3 checkMaskCamPos;
+            if (!HitTestUVPosition(cursorRay, ref uvWorldPosition, maskPainterCams[(int) MaskNames.Master])) return;
+            if (!Physics.Raycast(maskPainterCams[(int) MaskNames.Master].transform.position + uvWorldPosition, Vector3.forward, out var hitMaster)) return;
+            
             // determine which mask cam to check based on master decal tag
-            Vector3 checkMaskCamPos = Vector3.zero;
-            if (masterDecalTag == MaskNames.Red.ToString())
+            if (hitMaster.collider.CompareTag(MaskNames.Red.ToString()))
             {
                 checkMaskCamPos = maskPainterCams[(int) MaskNames.Red].transform.position;
             }
-            else if (masterDecalTag == MaskNames.Green.ToString())
+            else if (hitMaster.collider.CompareTag(MaskNames.Green.ToString()))
             {
                 checkMaskCamPos = maskPainterCams[(int) MaskNames.Green].transform.position;
             }
@@ -107,12 +124,30 @@ public class TerrainPainter : MonoBehaviour
             {
                 checkMaskCamPos = maskPainterCams[(int) MaskNames.Blue].transform.position;
             }
-
+                    
             // check mask cam for mask cam decal tag and send to terrain select
-            if (Physics.Raycast(checkMaskCamPos, Vector3.forward, out var hit))
+            if (!Physics.Raycast(checkMaskCamPos + uvWorldPosition, Vector3.forward, out var hitMask)) return;
+            switch (terrainToolSelected)
             {
-                //selectedTerrain = ts.SelectedTerrain(masterDecalTag, hit.collider.tag, true);
+                case TerrainTools.Plants:
+                    selectedTerrain = ts.SelectedTerrain(masterDecalTag, hitMask.collider.tag, false);
+                    break;
+                case TerrainTools.Up:
+                    selectedTerrain = ts.SelectedTerrain(masterDecalTag, hitMask.collider.tag, true);
+                    break;
             }
+        }
+    }
+
+    /// <summary>
+    /// Instantiates selected terrain on the planet at the hit position 
+    /// </summary>
+    /// <param name="cursorRay"></param>
+    private void SpawnTerrainOnPlanet(Ray cursorRay)
+    {
+        if (Physics.Raycast(cursorRay, out var hit , 1000, 9))
+        {
+            Instantiate(selectedTerrain, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
         }
     }
     
@@ -122,7 +157,6 @@ public class TerrainPainter : MonoBehaviour
     /// <param name="cursorRay">The Ray of the hit on the object</param>
     /// <param name="uvWorldPosition">The Vector3 to track the position of the hit on the UV</param>
     /// <param name="maskCam">The Camera of the UV mask</param>
-    /// <param name="decalTag">The tag of the color decal hit</param>
     /// <returns></returns>
     private bool HitTestUVPosition(Ray cursorRay, ref Vector3 uvWorldPosition, Camera maskCam)
     {
@@ -135,10 +169,8 @@ public class TerrainPainter : MonoBehaviour
             uvWorldPosition.z = 0.0f;
             return true;
         }
-        else
-        {
-            return false;
-        }
+        
+        return false;
     }
     
     /// <summary>
@@ -147,6 +179,6 @@ public class TerrainPainter : MonoBehaviour
     /// <param name="tool">Terrain Feature Type</param>
     public void TerrainOption(string tool)
     {
-        terrainToolSelected = (terrainTools)System.Enum.Parse(typeof(terrainTools), tool);
+        terrainToolSelected = (TerrainTools)System.Enum.Parse(typeof(TerrainTools), tool);
     }
 }
--- Nebula/Assets/Scripts/Planets/Terrestrial/TerrainSelect.cs
+++ Nebula/Assets/Scripts/Planets/Terrestrial/TerrainSelect.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class TerrainSelect : MonoBehaviour
 {
     /// <summary>
+    /// The names of the biome masks
+    /// </summary>
+    private enum MaskNames {Red, Green, Blue};
+    
+    /// <summary>
     /// The names of the terrainObjects in order.
     /// WILL NEED TO CHANGE WITH FINAL ART!
     /// </summary>
-    private enum terrainNames
+    private enum TerrainNames
     {
         Pine,
         PineSnowy,
@@ -33,9 +38,62 @@ public class TerrainSelect : MonoBehaviour
     [SerializeField, Tooltip("Array of terrain objects that can be painted.")]
     private GameObject[] terrainObjects;
 
-    
+    /// <summary>
+    /// Selects terrain to spawn based on masterDecalTag, maskDecalTag, and isUp
+    /// </summary>
+    /// <param name="masterDecalTag">The color of the masterDecal</param>
+    /// <param name="maskDecalTag">The color of the maskDecal</param>
+    /// <param name="isUp">Is the terrain up or plant</param>
+    /// <returns></returns>
     public GameObject SelectedTerrain(string masterDecalTag, string maskDecalTag, bool isUp)
     {
-        return null;
+        if (masterDecalTag == MaskNames.Red.ToString())
+        {
+            if (maskDecalTag == MaskNames.Red.ToString()) // plains
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.Rock] : terrainObjects[(int) TerrainNames.Bush];
+            }
+            else if (masterDecalTag == MaskNames.Green.ToString()) // savanna
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.RockDesert] : terrainObjects[(int) TerrainNames.Cacti];
+            }
+            else // tropical
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.Plateau] : terrainObjects[(int) TerrainNames.Cacti];
+            }
+        }
+        else if (masterDecalTag == MaskNames.Green.ToString())
+        {
+            if (maskDecalTag == MaskNames.Red.ToString()) // temperate
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.Mountain] : terrainObjects[(int) TerrainNames.PineSnowy];
+            }
+            else if (masterDecalTag == MaskNames.Green.ToString()) // coniferous
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.Hill] : terrainObjects[(int) TerrainNames.Pine];
+            }
+            else // taiga
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.HillSnowy] : terrainObjects[(int) TerrainNames.PineSnowy];
+            }
+        }
+        else if (masterDecalTag == MaskNames.Blue.ToString())
+        {
+            if (maskDecalTag == MaskNames.Red.ToString()) // ocean
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.Volcano] : terrainObjects[(int) TerrainNames.Island];
+            }
+            else // ice
+            {
+                return isUp ? terrainObjects[(int) TerrainNames.IceChunk] : terrainObjects[(int) TerrainNames.PineSnowy];
+            }
+        }
+        else
+        {
+            Debug.LogError("Could not select terrain. MasterDecalTag: " + masterDecalTag + 
+                                                                " MaskDecalTag: " + maskDecalTag + 
+                                                                " isUp: " + isUp);
+            return null;
+        }
     }
 }
