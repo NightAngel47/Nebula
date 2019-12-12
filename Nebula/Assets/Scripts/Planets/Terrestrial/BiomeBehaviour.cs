0de9cbda986d71350fb7d5eb8ba369be9035fdd1
Steven <NightAngel47@users.noreply.github.com>
2019-12-11 18:36:09 -0600

    New Terrestrial Planet with biome Wrapping WIP
    Started implementing new terrestrial planet along with biome wrapping at seams. Also, updated biome color wheel with new colors.

---
+++ Nebula/Assets/Scripts/Planets/Terrestrial/BiomeBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiomeBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// Does the biome have a copy
+    /// </summary>
+    public bool hasCopy = false;
+}
---
+++ Nebula/Assets/Scripts/Planets/Terrestrial/BiomeWrapping.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiomeWrapping : MonoBehaviour
+{
+    public Transform otherEdge;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        BiomeBehaviour originalBiome = other.GetComponent<BiomeBehaviour>();
+        if (originalBiome != null && !originalBiome.hasCopy)
+        {
+            print("copied");
+            
+            // spawn copy
+            var otherEdgePosition = otherEdge.position;
+            Vector3 newPos = otherEdgePosition + new Vector3(0, otherEdgePosition.y - other.transform.position.y, 0);
+            GameObject copy = Instantiate(other.gameObject, newPos, Quaternion.identity, other.transform.parent);
+            
+            // clean up
+            Destroy(originalBiome);
+            BiomeBehaviour copyData = copy.GetComponent<BiomeBehaviour>();
+            if (copyData != null)
+            {
+                Destroy(copyData);
+            }
+        }
+    }
+}
